5b00a04632cdf1d685ca3e1609e01c222a9e0ce2
ENNCELADUS <2162352828@qq.com>
2024-05-10 23:59:35 +0800

    move constructor and move operator not finished

--- hw5/prob2/attachments/dynarray.hpp
+++ hw5/prob2/attachments/dynarray.hpp
@@ -4,6 +4,97 @@
 #include <cstddef>
 #include <stdexcept>
 
-class Dynarray {};
+class Dynarray {
+    std::size_t m_size = 0;
+    int *m_storage = nullptr;
+public:
+    // Initialization 
+    // Default constructor
+    Dynarray() = default;
 
-#endif // DYNARRAY_HPP
+    // Initialize with a length (avoid implicitly conversion)
+    explicit Dynarray(std::size_t n)
+        : m_size(n), m_storage(new int[n]{}){}
+
+    // Initialize with length n and value x
+    Dynarray(size_t n, int x)
+        : m_size(n), m_storage(new int[n]{}){
+        for (auto i = 0; i < n; i ++){
+            m_storage[i] = x;
+        }
+    }
+
+    // Initializes the object a to be an "array" of length end - begin
+    Dynarray(const int *begin, const int *end)
+        : m_size(end - begin), m_storage(new int[m_size]{}){
+        const int *ptr = begin;
+        for (auto i = 0; i < m_size; i++){
+            m_storage[i] = *ptr;
+            ptr++;
+        }
+    }
+
+    // Copy-initialization
+    Dynarray(const Dynarray &other)
+        : m_size(other.size()), m_storage(new int[m_size]{}){
+        for (auto i = 0; i < m_size; i++){
+            m_storage[i] = other.at(i);
+        }
+    };
+
+    // Copy operator(version 1)
+    Dynarray &operator=(const Dynarray &other){
+        int *new_storage = new int[other.size()];
+        for (auto i = 0; i < other.size(); i++){
+            new_storage[i] = other.at(i);
+        }
+        delete[] m_storage;
+        m_size = other.size();
+        m_storage = new_storage;
+        return *this;
+    }
+
+    // Another possible version of copy operator, but not handle self-assignment.
+
+    // Dynarray &operator=(const Dynarray &other){
+    //     if (this != &other){
+    //         delete[] m_storage;
+    //         m_size = other.size();
+    //         m_storage = new int[m_size];
+    //         for (auto i = 0; i < other.size(); i++)
+    //         {
+    //             m_storage[i] = other.at(i);
+    //         }
+    //     }
+    //     return *this;
+    // }
+
+    // Initialization through a move
+
+
+    // Destructor
+    ~Dynarray() {
+        delete[] m_storage;
+    }
+
+    // Basic info
+    std::size_t size() const { return m_size; }
+    bool empty() const { return m_size == 0u; }
+
+    // Element access
+    int &at(size_t n){
+        if (n >= m_size){
+            throw std::out_of_range{"Dynarray index out of range!"};
+        }
+        return m_storage[n];
+    }
+
+    const int &at(size_t n) const {
+        if (n >= m_size){
+            throw std::out_of_range{"Dynarray index out of range!"};
+        }
+        return m_storage[n];
+    }
+};
+
+#endif // DYNARRAY_HPP 
