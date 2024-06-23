9bcb51dfa1784cc0384b390ab5ba24c031d292a5
ENNCELADUS <2162352828@qq.com>
2024-06-23 23:29:37 +0800

    GameObject

--- hw8/attachments/include/pvz/GameObject/GameObject.hpp
+++ hw8/attachments/include/pvz/GameObject/GameObject.hpp
@@ -8,12 +8,37 @@
 class GameWorld;
 using pGameWorld = std::shared_ptr<GameWorld>;
 
+/**
+ * @brief The GameObject class, inheriting from ObjectBase and std::enable_shared_from_this.
+ * Represents a generic game object that can be managed in the game world.
+ */
 class GameObject : public ObjectBase, public std::enable_shared_from_this<GameObject> {
 public:
   using std::enable_shared_from_this<GameObject>::shared_from_this;
 
-private:
+  /**
+   * @brief Construct a new Game Object object
+   * 
+   * @param imageID The image ID for the game object.
+   * @param x The x-coordinate of the game object.
+   * @param y The y-coordinate of the game object.
+   * @param layer The display layer of the game object.
+   * @param width The width of the game object.
+   * @param height The height of the game object.
+   * @param animID The animation ID for the game object.
+   */
+  GameObject(ImageID imageID, int x, int y, LayerID layer, int width, int height, AnimID animID);
+  virtual ~GameObject() = default;
 
+  // Pure virtual function for updating the game object
+  virtual void Update() = 0;
+
+  // Functions to check if the object is dead and to mark it as dead
+  bool IsDead() const;
+  void MarkAsDead();
+
+private:
+  bool isDead;
 };
 
 
--- hw8/attachments/src/GameObject/GameObject.cpp
+++ hw8/attachments/src/GameObject/GameObject.cpp
@@ -1,3 +1,32 @@
 #include "pvz/GameObject/GameObject.hpp"
 
-// Your everything begins from here.
+/**
+ * @brief Construct a new GameObject object.
+ * 
+ * @param imageID The image ID for the game object.
+ * @param x The x-coordinate of the game object.
+ * @param y The y-coordinate of the game object.
+ * @param layer The display layer of the game object.
+ * @param width The width of the game object.
+ * @param height The height of the game object.
+ * @param animID The animation ID for the game object.
+ */
+GameObject::GameObject(ImageID imageID, int x, int y, LayerID layer, int width, int height, AnimID animID)
+    : ObjectBase(imageID, x, y, layer, width, height, animID), isDead(false) {
+}
+
+/**
+ * @brief Check if the game object is dead.
+ * 
+ * @return true if the game object is dead, false otherwise.
+ */
+bool GameObject::IsDead() const {
+    return isDead;
+}
+
+/**
+ * @brief Mark the game object as dead.
+ */
+void GameObject::MarkAsDead() {
+    isDead = true;
+}
