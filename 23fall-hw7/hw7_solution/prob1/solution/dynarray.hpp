896f7d656217c0e33581dc09297fa8c33fc650a8
ENNCELADUS <2162352828@qq.com>
2025-01-17 12:12:04 +0800

    Update dynarray.hpp

--- 23fall-hw7/hw7_solution/prob1/solution/dynarray.hpp
+++ 23fall-hw7/hw7_solution/prob1/solution/dynarray.hpp
@@ -3,8 +3,10 @@
 
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <stdexcept>
 #include <utility>
+#include <type_traits>
 
 template <typename T>
 class Dynarray
@@ -34,74 +36,82 @@ private:
 public:
   explicit Dynarray(size_type n)
       : m_length{n}, m_storage{new value_type[n]{}} {}
+
   Dynarray(size_type n, const value_type &x)
       : m_length{n}, m_storage{new value_type[n]}
   {
     std::fill_n(m_storage, n, x);
   }
-  Dynarray() : m_length{0}, m_storage{nullptr} {}
+
+  Dynarray() noexcept : m_length{0}, m_storage{nullptr} {}
+
   template <typename ForwardIter,
             typename = std::enable_if_t<std::is_base_of_v<
                 std::forward_iterator_tag,
                 typename std::iterator_traits<ForwardIter>::iterator_category>>>
   Dynarray(ForwardIter begin, ForwardIter end)
-      : m_length(std::distance(begin, end)),
+      : m_length(static_cast<size_type>(std::distance(begin, end))),
         m_storage{new value_type[m_length]}
   {
     std::copy(begin, end, m_storage);
   }
+
   Dynarray(const Dynarray &other)
-      : m_length{other.size()}, m_storage{new value_type[m_length]}
+      : m_length{other.size()}, m_storage{new value_type[other.size()]}
   {
     std::copy_n(other.m_storage, other.m_length, m_storage);
   }
+
   Dynarray(Dynarray &&other) noexcept
       : m_length{std::exchange(other.m_length, 0)},
         m_storage{std::exchange(other.m_storage, nullptr)} {}
+
+  ~Dynarray() { delete[] m_storage; }
+
+  Dynarray &operator=(Dynarray other) noexcept
+  {
+    swap(other);
+    return *this;
+  }
+
   void swap(Dynarray &other) noexcept
   {
     using std::swap;
     swap(m_length, other.m_length);
     swap(m_storage, other.m_storage);
   }
-  Dynarray &operator=(Dynarray other) noexcept
-  {
-    swap(other);
-    return *this;
-  }
-  ~Dynarray() { delete[] m_storage; }
 
-  iterator begin() { return m_storage; }
-  iterator end() { return m_storage + m_length; }
-  const_iterator cbegin() const { return m_storage; }
-  const_iterator cend() const { return m_storage + m_length; }
-  const_iterator begin() const { return cbegin(); }
-  const_iterator end() const { return cend(); }
-  reverse_iterator rbegin() { return reverse_iterator(end()); }
-  reverse_iterator rend() { return reverse_iterator(begin()); }
-  const_reverse_iterator crbegin() const
-  {
-    return const_reverse_iterator(cend());
-  }
-  const_reverse_iterator crend() const
-  {
-    return const_reverse_iterator(cbegin());
-  }
-  const_reverse_iterator rbegin() const { return crbegin(); }
-  const_reverse_iterator rend() const { return crend(); }
+  iterator begin() noexcept { return m_storage; }
+  const_iterator begin() const noexcept { return cbegin(); }
+  const_iterator cbegin() const noexcept { return m_storage; }
 
-  size_type size() const { return m_length; }
-  bool empty() const { return m_length == 0u; }
+  iterator end() noexcept { return m_storage + m_length; }
+  const_iterator end() const noexcept { return cend(); }
+  const_iterator cend() const noexcept { return m_storage + m_length; }
+
+  reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+  const_reverse_iterator rbegin() const noexcept { return crbegin(); }
+  const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(cend()); }
+
+  reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+  const_reverse_iterator rend() const noexcept { return crend(); }
+  const_reverse_iterator crend() const noexcept { return const_reverse_iterator(cbegin()); }
+
+  constexpr size_type size() const noexcept { return m_length; }
+  constexpr bool empty() const noexcept { return m_length == 0u; }
+
   reference at(size_type n)
   {
     return const_cast<reference>(static_cast<const Dynarray *>(this)->at(n));
   }
+
   const_reference at(size_type n) const
   {
     if (n >= m_length)
       throw std::out_of_range{"Dynarray index out of range."};
     return m_storage[n];
   }
+
   reference operator[](size_type n) { return m_storage[n]; }
   const_reference operator[](size_type n) const { return m_storage[n]; }
 
@@ -110,14 +120,16 @@ public:
     return std::equal(lhs.m_storage, lhs.m_storage + lhs.m_length,
                       rhs.m_storage, rhs.m_storage + rhs.m_length);
   }
+
   friend bool operator<(const Dynarray &lhs, const Dynarray &rhs)
   {
     return std::lexicographical_compare(
-        lhs.m_storage, lhs.m_storage + lhs.m_length, rhs.m_storage,
-        rhs.m_storage + rhs.m_length);
+        lhs.m_storage, lhs.m_storage + lhs.m_length,
+        rhs.m_storage, rhs.m_storage + rhs.m_length);
   }
 };
 
+// Non-member Relational Operators
 template <typename T>
 bool operator!=(const Dynarray<T> &lhs, const Dynarray<T> &rhs)
 {
@@ -143,6 +155,14 @@ bool operator>=(const Dynarray<T> &lhs, const Dynarray<T> &rhs)
 }
 
 template <typename T>
+void swap(Dynarray<T> &lhs, Dynarray<T> &rhs) noexcept
+{
+  lhs.swap(rhs);
+}
+
+// Output Operator with SFINAE constraint for streamability
+template <typename T, typename = std::enable_if_t<
+                          std::is_same_v<decltype(std::declval<std::ostream &>() << std::declval<T>()), std::ostream &>>>
 std::ostream &operator<<(std::ostream &os, const Dynarray<T> &a)
 {
   os << '[';
@@ -155,6 +175,7 @@ std::ostream &operator<<(std::ostream &os, const Dynarray<T> &a)
   return os << ']';
 }
 
+// Deduction Guide at namespace scope (C++17 and later)
 template <typename ForwardIter>
 Dynarray(ForwardIter, ForwardIter)
     -> Dynarray<typename std::iterator_traits<ForwardIter>::value_type>;
